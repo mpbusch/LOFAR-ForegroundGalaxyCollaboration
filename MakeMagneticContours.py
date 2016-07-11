5eb449a245f729e6b425c7585ff8be5728aa8734
Michael Busch <mpbusch@users.noreply.github.com>
2016-07-10 21:35:38 -0700

    Update MakeMagneticContours.py

--- MakeMagneticContours.py
+++ MakeMagneticContours.py
@@ -16,10 +16,12 @@ __author__ = "Michael Busch"
 __license__ = "MIT License"
 __version__ = "1.0.0"
 __maintainer__ = "Michael Busch"
-__email__ = "mpbusch@asu.edu"
+__email__ = "mpbusch@jhu.edu"
 __status__ = "Production"
 
 from astropy.io import fits
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -28,10 +30,24 @@ import numpy as np
 infile = 'M63_spix_rmscut.fits'
 outfile = 'M63_magneticfield.fits'
 
-
+def plot_data(data):
+  ''' Plots Fits Data for simple inspection '''
+  plt.imshow(data)
+  plt.title('Image Data')
+  plt.colorbar()
+  plt.show()
+  
+def print_info(infile):
+  hdu_list = fits.open(infile)
+  hdu_list.info()
+  return print(hdu_list.info())
 
 if __name__ == "__main__":
-  data, header = fits.getdata(infile, header=True)
+  image_data = hdu_list[0].data[:,:,0,0] # Check axis from hdu_list.info()
+  print(type(image_data))
+  print(image_data.shape()) # Check to make sure this makes sense. Can use np.swapaxes(array, indx, indx) if needed.
+  
+  
   
 
 
