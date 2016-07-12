a0b5230c4349c2856d054b6c5288ee4220e772b3
Michael Busch <mpbusch@users.noreply.github.com>
2016-07-11 18:09:39 -0700

    Added spectral index and intensity inputs
    Added spectral index and intensity inputs, changed B_calc loop to reflect we are getting information from two fits files, and outputting to one fits file.
    
    Added Comments
    
    Corrected way to determine shape of data.
    
    A lot of values will be NaN, I wonder what runtime error Python will complain about?

--- MakeMagneticContours.py
+++ MakeMagneticContours.py
@@ -28,31 +28,37 @@ import scipy
 
 #### Global Constants ### 
 # Please change as needed.
-infile = 'M63_spix_rmscut.fits'
+spix_infile = 'M63_spix_rmscut.fits'    # Change to spectral index map of your galaxy.
+intensity_infile = 'M63_LOFAR_convolve2_cut_reorder_regrid.fits'  # Change to intensity map for your galaxy.
 outfile = 'M63_magneticfield.fits' 
-  rows,columns=infile.shape
-  Bv=np.zeros_like(infile)
-  K0=100 
-  Ep=938.28                 #Mev or 1.5033*10**-3 erg 
-  inclination=              #user input
-  length=                   #user input? has to be assumed 
-  frequency=                #user input
-  #alpha defined in loop 
-  #gamma defined in loop
-  c1=6.26428e18             #erg**-2 s**-1 G**-1 
-  #c2 defined in loop (we can make a function of this if wanted instead)
-  c3=1.86558e(-23)          #erg G**-1 sterad**-1
-  c4=(np.cos(inclination))**((gamma+1)/2)
+get_shape = fits.open(spix_infile)      # These commands are needed to correctly extract data from a .fits file
+file_shape = get_shape[0].data[0,0,:,:] # Unpacking data from .fits file to determine shape.
+rows,columns= file_shape.shape          # Determining number of columns and rows.
+Bv=np.zeros_like(file_shape)            # Array of shape with same dimensions as data.
+K0=100                                  # Proton-electron number density ration, 100 assumed for star forming regions.
+Ep=938.28                               # Mev or 1.5033*10**-3 erg 
+inclination=55                          # user input, change for each galaxy
+l=                                      # l, user input? has to be assumed, 'pathlength'
+v=131                                   # v, frequency in MHz, user input
+#alpha defined in loop 
+#gamma defined in loop
+c1=6.26428e18                           #erg**-2 s**-1 G**-1 
+#c2 defined in loop (we can make a function of this if wanted instead)
+c3=1.86558e(-23)                        #erg G**-1 sterad**-1
+c4=(np.cos(inclination))**((gamma+1)/2)
   
 
-def B_calc(infile): 
+def B_calc(spix_infile, intensity_infile): 
+  alpha_list = fits.open(spix_infile)
+  intensity_list = fits.open(intensity_infile)
   for i in range(0,rows): 
     for j in range(0,columns):
-      gamma= #UPDATE
-      alpha= (gamma-1)/2    #this may be a special case only, synchrotron spectral index
+      alpha=alpha_list[0].data[0,0,i,j]   #Spectral Index per pixel from Spectral Index Map
+      gamma= 2*alpha + 1                  #I guess? Please check this.
+      #alpha= (gamma-1)/2                 #This may be a special case only, synchrotron spectral index
       c2= .25*c3*(gamma+(7/3))/(gamma+1)*scipy.gamma((3*gamma-1)/12)*scipy.gamma((3*gamma+7)/12)
-      Iv=infile[i,j] 
-      Bv[i,j]=(4*np.pi*(2*alpha+1)*(K0+1)*Iv*Ep**(1-2*alpha)*(v/(2*c1))**alpha/((2*alpha-1)*c2*l*c4))**(1/(alpha+3)) 
+      Iv=intensity_list[0].data[0,0,i,j] #Intensity per pixel from Intensity Map
+      Bv[i,j]=(4*np.pi*(2*alpha+1)*(K0+1)*Iv*Ep**(1-2*alpha)*(v/(2*c1))**alpha/((2*alpha-1)*c2*l*c4))**(1/(alpha+3))
   return Bv
       
 
@@ -69,12 +75,7 @@ def print_info(infile):
   return print(hdu_list.info())
 
 if __name__ == "__main__":
-  image_data = hdu_list[0].data[:,:,0,0] # Check axis from hdu_list.info()
+  image_data = hdu_list[0].data[0,0,:,:] # Check axis from print_info()
   print(type(image_data))
-  print(image_data.shape()) # Check to make sure this makes sense. Can use np.swapaxes(array, indx, indx) if needed.
+  print(image_data.shape()) # Check to make sure this makes sense. Can use np.swapaxes(array, indx, indx) if needed
   
-  
-  
-
-
-
